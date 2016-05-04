4e8430cb69b49f54905604c78344879b5f3ed54e
kyly <kyly1982@gmail.com>
2016-05-04 18:17:20 +0800

    调整pk通知，将发消息改为发自定义的通知

--- app/src/main/java/com/mckuai/imc/Fragment/MainFragment_Competition.java
+++ app/src/main/java/com/mckuai/imc/Fragment/MainFragment_Competition.java
@@ -2,22 +2,32 @@ package com.mckuai.imc.Fragment;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.mckuai.imc.Activity.CartoonActivity;
 import com.mckuai.imc.Activity.MainActivity;
 import com.mckuai.imc.Activity.UserCenterActivity;
 import com.mckuai.imc.Base.BaseFragment;
 import com.mckuai.imc.Base.MCKuai;
+import com.mckuai.imc.Bean.Ad;
 import com.mckuai.imc.Bean.Cartoon;
+import com.mckuai.imc.Bean.MCCartoonPKNotificationMessage;
 import com.mckuai.imc.Bean.Page;
 import com.mckuai.imc.Bean.User;
+import com.mckuai.imc.DownloadService;
 import com.mckuai.imc.R;
 import com.mckuai.imc.Util.MCNetEngine;
 import com.mckuai.imc.Widget.CompetitionLayout;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 import com.umeng.analytics.MobclickAgent;
 
 import java.util.ArrayList;
@@ -26,24 +36,36 @@ import io.rong.imkit.RongIM;
 import io.rong.imlib.RongIMClient;
 import io.rong.imlib.model.Conversation;
 import io.rong.imlib.model.Message;
-import io.rong.message.TextMessage;
 
 
-public class MainFragment_Competition extends BaseFragment implements CompetitionLayout.OnActionListener,MCNetEngine.OnLoadCartoonListResponseListener,MCNetEngine.OnRewardCartoonResponseListener {
+public class MainFragment_Competition extends BaseFragment implements CompetitionLayout.OnActionListener
+        ,MCNetEngine.OnLoadCartoonListResponseListener
+        ,MCNetEngine.OnRewardCartoonResponseListener
+        ,View.OnClickListener
+        ,MCNetEngine.OnGetAdsResponse{
     private View view;
     private CompetitionLayout competitionLayout;
     private ArrayList<Cartoon> cartoons;
+    private RelativeLayout adRoot;
+    private ImageView adView;
+    private AppCompatButton adCloseBtn;
+
     private Page page;
     private Cartoon voteCartoon;
     private Cartoon failCartoon;
 
     private MCKuai application;
 
+    private long lastAdShowTime = 0;
+    private int lastAdInterval = 0;
+    private int adShowCount = 0;
+    private ArrayList<Ad> ads;
+    private ImageLoader loader;
+
 
     public MainFragment_Competition() {
         application = MCKuai.instence;
     }
-
 
 
     @Override
@@ -60,42 +82,55 @@ public class MainFragment_Competition extends BaseFragment implements Competitio
     public void onResume() {
         super.onResume();
 
-        if (null == page){
+        if (null == page) {
             loadData();
+        }
+        if (null == ads){
+            application.netEngine.getAds(getActivity(),this);
         }
     }
 
     @Override
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
-        if (!hidden){
+        if (!hidden) {
             showData();
+        }
+        if (lastAdShowTime > 0){
+            long time = System.currentTimeMillis();
+            lastAdShowTime = time - (time - lastAdShowTime);
         }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 2 && resultCode == Activity.RESULT_OK){
-            onVote(voteCartoon,failCartoon);
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK) {
+            onVote(voteCartoon, failCartoon);
         }
     }
 
-    private void initView(){
+    private void initView() {
         competitionLayout = (CompetitionLayout) view.findViewById(R.id.competition);
+        adRoot = (RelativeLayout) view.findViewById(R.id.adRoot);
+        adView = (ImageView) view.findViewById(R.id.ad);
+        adCloseBtn = (AppCompatButton) view.findViewById(R.id.adCloseButton);
+
         competitionLayout.setActionListener(this);
+        adView.setOnClickListener(this);
+        adCloseBtn.setOnClickListener(this);
     }
 
 
-    private void loadData(){
-        if (null == page){
-            page= new Page();
+    private void loadData() {
+        if (null == page) {
+            page = new Page();
         }
         application.netEngine.loadCartoonList(getActivity(), page, this);
     }
 
-    private void showData(){
-        if (null == page){
+    private void showData() {
+        if (null == page) {
             loadData();
         } else {
             if (null != cartoons) {
@@ -104,6 +139,119 @@ public class MainFragment_Competition extends BaseFragment implements Competitio
         }
     }
 
+    private void showAd() {
+        if (check()){
+            if (null != ads && null != ads.get(adShowCount) && null != ads.get(adShowCount).getBitmap()){
+                MobclickAgent.onEvent(getActivity(),"IAD_ShowAd");
+                adView.setImageBitmap(ads.get(adShowCount ).getBitmap());
+                adView.setTag(ads.get(adShowCount));
+
+                lastAdShowTime = System.currentTimeMillis();
+                adRoot.setVisibility(View.VISIBLE);
+                adShowCount++;
+                lastAdInterval = 0;
+                if (adShowCount < ads.size()) {
+                    preLoadAdBitmap();
+                }
+            }
+        }
+    }
+
+    private boolean check() {
+//        Log.e("check",null == ads ? "广告为空":"adshowcount="+adShowCount +", lastTime="+lastAdShowTime +", lastAdInterval="+lastAdInterval);
+        if (null != ads && ads.size() > adShowCount && adShowCount < 2) {
+            lastAdInterval++;
+            if (lastAdInterval > 2) {//最多允许每三次
+                long time = System.currentTimeMillis();
+                if (time - lastAdShowTime > 30000) {
+                    double d = Math.random() * 10;
+                    d += Math.pow(lastAdInterval / 4, 3);
+//                    Log.e("check","ad="+d);
+                    return d > 10;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void preLoadAdBitmap(){
+        if (null != ads && ads.size() > adShowCount && null != ads.get(adShowCount)){
+            if (null == loader){
+                loader = ImageLoader.getInstance();
+            }
+            loader.loadImage(ads.get(adShowCount).getImageUrl(), new ImageLoadingListener() {
+                @Override
+                public void onLoadingStarted(String imageUri, View view) {
+
+                }
+
+                @Override
+                public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+
+                }
+
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    ads.get(adShowCount).setBitmap(loadedImage);
+                }
+
+                @Override
+                public void onLoadingCancelled(String imageUri, View view) {
+
+                }
+            });
+        }
+    }
+
+    private void sendPKNotification(Cartoon win,Cartoon lose){
+        if (null != RongIM.getInstance() && null != RongIM.getInstance().getRongIMClient()) {
+            //TextMessage message = TextMessage.obtain("恭喜你，你的漫画在与 \"" + lose.getOwner().getNickEx() + "\" 的漫画的PK中取得了胜利。");
+            MCCartoonPKNotificationMessage msg = new MCCartoonPKNotificationMessage(win,lose);
+
+            RongIM.getInstance().getRongIMClient().sendMessage(Conversation.ConversationType.SYSTEM, win.getOwner().getName(), msg, application.user.getNike() + "你的漫画在PK中取得了胜利！", "", new RongIMClient.SendMessageCallback() {
+                @Override
+                public void onError(Integer integer, RongIMClient.ErrorCode errorCode) {
+
+                }
+
+                @Override
+                public void onSuccess(Integer integer) {
+
+                }
+            }, new RongIMClient.ResultCallback<Message>() {
+                @Override
+                public void onSuccess(Message message) {
+
+                }
+
+                @Override
+                public void onError(RongIMClient.ErrorCode errorCode) {
+
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ad:
+                MobclickAgent.onEvent(getActivity(),"IAD_Click");
+                Ad ad = (Ad) v.getTag();
+                if (null != ad) {
+                    Intent intent = new Intent(getActivity(), DownloadService.class);
+                    intent.putExtra("URL",ad.getDownUrl());
+                    intent.putExtra("NAME",ad.getDownName());
+                    getActivity().startService(intent);
+                }
+                adRoot.setVisibility(View.GONE);
+                break;
+            case R.id.adCloseButton:
+                MobclickAgent.onEvent(getActivity(),"IAD_Close");
+                adRoot.setVisibility(View.GONE);
+                break;
+        }
+    }
 
     @Override
     public void EOF() {
@@ -114,46 +262,26 @@ public class MainFragment_Competition extends BaseFragment implements Competitio
     public void onShowDetile(Cartoon cartoon) {
         Intent intent = new Intent(getActivity(), CartoonActivity.class);
         Bundle bundle = new Bundle();
-        bundle.putSerializable(getString(R.string.cartoondetail_tag_cartoon),cartoon);
+        bundle.putSerializable(getString(R.string.cartoondetail_tag_cartoon), cartoon);
         intent.putExtras(bundle);
         getActivity().startActivity(intent);
     }
 
+
+
     @Override
-    public void onVote(Cartoon win,Cartoon fail) {
-        MobclickAgent.onEvent(getActivity(),"competitionCartoon");
+    public void onVote(Cartoon win, Cartoon lose) {
+        MobclickAgent.onEvent(getActivity(), "competitionCartoon");
 //        Log.e("onVote","win="+win.getId()+",fail="+fail.getId());
-        if (null != win && null != fail) {
+        if (null != win && null != lose) {
             if (application.isLogin()) {
-                MCKuai.instence.netEngine.rewardCartoon(getActivity(),application.user.getId(), win, this);
-                if (null != RongIM.getInstance() && null != RongIM.getInstance().getRongIMClient()) {
-                    TextMessage message = TextMessage.obtain("恭喜你，你的漫画在与 \""+fail.getOwner().getNickEx()+"\" 的漫画的PK中取得了胜利。");
-                    RongIM.getInstance().getRongIMClient().sendMessage(Conversation.ConversationType.PRIVATE, win.getOwner().getName(), message, application.user.getNike() + "你的漫画在PK中取得了胜利！", "", new RongIMClient.SendMessageCallback() {
-                        @Override
-                        public void onError(Integer integer, RongIMClient.ErrorCode errorCode) {
-
-                        }
-
-                        @Override
-                        public void onSuccess(Integer integer) {
-
-                        }
-                    }, new RongIMClient.ResultCallback<Message>() {
-                        @Override
-                        public void onSuccess(Message message) {
-
-                        }
-
-                        @Override
-                        public void onError(RongIMClient.ErrorCode errorCode) {
-
-                        }
-                    });
-                }
+                MCKuai.instence.netEngine.rewardCartoon(getActivity(), application.user.getId(), win, this);
+                sendPKNotification(win,lose);
+                showAd();
             } else {
                 voteCartoon = win;
-                failCartoon = fail;
-                ((MainActivity)getActivity()).callLogin(2);
+                failCartoon = lose;
+                ((MainActivity) getActivity()).callLogin(2);
             }
         }
     }
@@ -161,7 +289,7 @@ public class MainFragment_Competition extends BaseFragment implements Competitio
     @Override
     public void onShowUser(User user) {
         Intent intent = new Intent(getActivity(), UserCenterActivity.class);
-        intent.putExtra(getString(R.string.usercenter_tag_userid),user.getId().intValue());
+        intent.putExtra(getString(R.string.usercenter_tag_userid), user.getId().intValue());
         getActivity().startActivity(intent);
     }
 
@@ -177,19 +305,19 @@ public class MainFragment_Competition extends BaseFragment implements Competitio
 
     @Override
     public void onLoadCartoonListSuccess(ArrayList<Cartoon> cartoons) {
-        page.setPage(page.getPage()+1);
+        page.setPage(page.getPage() + 1);
         competitionLayout.setData(cartoons);
     }
 
     @Override
     public void onRewardaCartoonFailure(String msg) {
-        if (msg.equals("已赞")){
-            MobclickAgent.onEvent(getActivity(),"competitionCartoon_FR");
+        if (msg.equals("已赞")) {
+            MobclickAgent.onEvent(getActivity(), "competitionCartoon_FR");
 //            Log.e("onReward","Failure_FR");
             competitionLayout.showData();
             return;
         }
-        showMessage(msg,null,null);
+        showMessage(msg, null, null);
         MobclickAgent.onEvent(getActivity(), "competitionCartoon_F");
 //        Log.e("onReward","Failure");
         competitionLayout.showData();
@@ -197,8 +325,19 @@ public class MainFragment_Competition extends BaseFragment implements Competitio
 
     @Override
     public void onRewardCartoonSuccess() {
-        MobclickAgent.onEvent(getActivity(),"competitionCartoon_S");
+        MobclickAgent.onEvent(getActivity(), "competitionCartoon_S");
 //        Log.e("onReward","Success");
         competitionLayout.showData();
+    }
+
+    @Override
+    public void onGetAdFailure(String msg) {
+
+    }
+
+    @Override
+    public void onGetAdSuccess(ArrayList<Ad> ads) {
+        this.ads = ads;
+        preLoadAdBitmap();
     }
 }
